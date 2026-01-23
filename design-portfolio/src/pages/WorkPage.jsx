24b57d2e862243fef21f339bda542b8540e04eda
hannahschwen <hannahschwen@gmail.com>
2026-01-23 17:01:32 -0500

    Design shadows

--- design-portfolio/src/pages/WorkPage.jsx
+++ design-portfolio/src/pages/WorkPage.jsx
@@ -1,9 +1,12 @@
 function WorkPage() {
   return (
     <>
-    <h1>Work</h1>
+      <h1 className="shadow-hug">Work</h1>
+
+      <div className="shadow-box">shadow test</div>
+      <img className="shadow-hug" src="/src/assets/flower-1-03.png"></img>
     </>
-  )
-};
+  );
+}
 
 export default WorkPage;
