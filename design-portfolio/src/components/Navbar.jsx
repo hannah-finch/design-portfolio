21d6782bb6befaf83475303f4a5e947c98677a09
hannahschwen <hannahschwen@gmail.com>
2026-01-24 12:51:27 -0500

    design navbar

--- design-portfolio/src/components/Navbar.jsx
+++ design-portfolio/src/components/Navbar.jsx
@@ -1,5 +1,13 @@
 function Navbar() {
-  return <>Navbar</>;
+  return (
+    <>
+      <nav className="flex-row padding-h-10per tab-line shadow-box-top">
+        <div className="tab shadow-box-top">one</div>
+        <div className="tab shadow-box-top tab-active">two</div>
+        <div className="tab tab-accent shadow-box-top">three</div>
+      </nav>
+    </>
+  );
 }
 
 export default Navbar;
