1cbbe4e05228866bb7f2cef880675d88536b515f
hannahschwen <hannahschwen@gmail.com>
2026-01-27 14:03:31 -0500

    simple footer

--- design-portfolio/src/components/Footer.jsx
+++ design-portfolio/src/components/Footer.jsx
@@ -1,10 +1,18 @@
 function Footer() {
   return (
-
     <footer>
-      Footer
-      </footer>
-  )
-};
+      <section
+        className="shadow-box"
+        style={{ backgroundColor: "var(--green-dark)", gap: "10px" }}
+      >
+        <h3 style={{ color: "black", opacity: ".4", textAlign: "center" }}>
+          Designed and coded by Hannah B Finch
+          <br />
+          Copyright © 2026 Hannah B Finch
+        </h3>
+      </section>
+    </footer>
+  );
+}
 
 export default Footer;
